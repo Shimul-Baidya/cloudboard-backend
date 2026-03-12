66b8449de6cba7a5d4ce479d35222246233471e0
Shimul-Baidya <shimul.cse31@gmail.com>
2026-03-12 10:29:08 +0600

    feat: add PostgreSQL configuration and database connection test

--- apps/main.py
+++ apps/main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
+from apps.database import engine
+from sqlalchemy import text
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup logic
+    try:
+        with engine.connect() as connection:
+            connection.execute(text("SELECT 1"))
+        print("Database connection successful")
+    except Exception as e:
+        print("Database connection failed")
+        print(e)
+
+    yield  
+
+
+app = FastAPI(lifespan=lifespan)
+
 
 @app.get("/health")
 def health():
     return {"status": "Okay"}
+
