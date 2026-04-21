f8d0fb4d6238ae1b65703e3e6e08c0b13a0f8c4f
Shimul-Baidya <shimul.cse31@gmail.com>
2026-04-21 15:13:27 +0600

    update auth.py: add exception methods

--- apps/services/auth.py
+++ apps/services/auth.py
@@ -1,10 +1,15 @@
 # authentication logic
 from sqlalchemy import false
+from starlette.status import HTTP_401_UNAUTHORIZED
 from apps.database import get_db
 from sqlalchemy.orm import Session
 from apps.models import user as user_model
-from fastapi import HTTPException
+from fastapi import HTTPException, status
 from apps.services.security import DUMMY_HASH, verify_password
+from fastapi.security import OAuth2PasswordBearer
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
 def get_user(db: Session, email: str):
@@ -27,3 +32,20 @@ def authenticate_user(db: Session, email: str, password: str):
     
 
 
+# exceptions
+
+def get_user_exception():
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+    return credentials_exception
+
+def token_exception():
+    token_exception_response = HTTPException(
+        status_code=HTTP_401_UNAUTHORIZED,
+        detail="Incorrect username or password",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+    return token_exception_response
